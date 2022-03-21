b99e51c34c3fb340d2546ffc65bfb37532fabe08
matt <mattevo10@gmail.com>
2022-03-21 12:44:42 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
-import Menu from "./components/menu/menu.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>i am the shop</h1>
+    </div>
+  );
+};
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Shoes",
-      imageUrl:
-        "https://images.unsplash.com/photo-1478827227954-745b0daf2534?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    },
-    {
-      id: 2,
-      title: "Chalk",
-      imageUrl:
-        "https://images.unsplash.com/photo-1550265486-df3659283974?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-    },
-    {
-      id: 3,
-      title: "Chalk Bag",
-      imageUrl:
-        "https://images.unsplash.com/photo-1473441732394-649138d63985?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    },
-    {
-      id: 4,
-      title: "Belay Device",
-      imageUrl:
-        "https://images.unsplash.com/photo-1586685256769-4e869a64f1eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-    },
-    {
-      id: 5,
-      title: "Ropes",
-      imageUrl:
-        "https://images.unsplash.com/photo-1635360404916-a7919e7de16f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    },
-  ];
-  return <Menu categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as Climbing } from "../../assets/mountainclimbing.svg";
+// import { ReactComponent as Climbing } from "../../assets/crown.svg";
+
+import "./navigations.styles.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <Climbing className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+export default Navigation;
