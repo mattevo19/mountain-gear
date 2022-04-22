c1246c2c26b57b0346509c648742a2b09d9ca671
matt <mattevo10@gmail.com>
2022-04-22 22:28:25 +0200

    context added for products

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/authentication/authentication.component";
 
-const Shop = () => {
-  return (
-    <div>
-      <h1>i am the shop</h1>
-    </div>
-  );
-};
+import Navigation from "./routes/navigation/navigation.component";
+import Home from "./routes/home/home.component";
+import Shop from "./routes/shop/shop.component";
+import Authentication from "./routes/authentication/authentication.component";
 
 const App = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from "./reportWebVitals";
 
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from "./contexts/products.context";
 
 import "./index.scss";
 
@@ -12,7 +13,9 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
