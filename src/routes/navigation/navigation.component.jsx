1450de820f767f58c4014b03fe1d321b169e9774
matt <mattevo10@gmail.com>
2022-04-27 19:09:52 +0200

    added icon for shopping cart + dropdown for items

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,8 +1,12 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import { ReactComponent as Climbing } from "../../assets/mountainclimbing.svg";
 // import { ReactComponent as Climbing } from "../../assets/crown.svg";
+
 import { UserContext } from "../../contexts/user.context";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
@@ -31,7 +35,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
